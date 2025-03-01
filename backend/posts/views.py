168dbfdc36650f712190f8bdc4d428999185320e
Kerciu <kerciuuu@gmail.com>
2025-03-01 19:28:49 +0100

    created rest api view for post details

--- backend/posts/views.py
+++ backend/posts/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Post
 
-# Create your views here.
+
+def post_details_view(request, id, *args, **kwargs):
+    data = {'id': id}
+    STATUS = 200
+
+    try:
+        obj = Post.objects.get(id=id)
+        data['title'] = obj.title
+        data['body'] = obj.body
+        data['image'] = obj.image.url
+        data['created_at'] = obj.created_at
+
+    except Post.DoesNotExist:
+        data['message'] = 'Not found'
+        STATUS = 404
+
+    data = {
+        'title': obj.title,
+        'body': obj.body,
+        'image': obj.image.url,
+        'created_at': obj.created_at,
+    }
+
+    return JsonResponse(data, status=STATUS)
