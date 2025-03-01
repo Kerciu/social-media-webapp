08f5a140ac6c29889be7fd986dfa2d470c4d1a41
Kerciu <kerciuuu@gmail.com>
2025-03-01 22:37:28 +0100

    fixed urls

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,15 @@
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
 
 
+@api_view(['GET'])
 def get_routes_view(request):
 
     routes = [
-        '/api/token',
-        '/api/token/refresh',
+        '/api/auth/token',
+        '/api/auth/token/refresh',
     ]
 
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
---
+++ backend/backend/views.py
@@ -0,0 +1,6 @@
+# backend/views.py
+from django.http import JsonResponse
+
+
+def home(request):
+    return JsonResponse({"message": "Welcome to the API!"})
