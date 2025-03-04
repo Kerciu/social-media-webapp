eae64f8cf8b1cca2360888fd36be86ae7e6a263b
Kerciu <kerciuuu@gmail.com>
2025-03-04 12:52:14 +0100

    started building user profile component

--- frontend/src/routes/userProfile.js
+++ frontend/src/routes/userProfile.js
@@ -1,7 +1,21 @@
-import { Text } from "@chakra-ui/react"
+import { Box, Flex, Text, VStack } from "@chakra-ui/react"
 
 export const UserProfile = () => {
-    return <>
-        <Text>User Profile</Text>
-    </>
+    return (
+        <Flex w='100%' justifyContent='center'>
+            <VStack w='75%' >
+                <Box w='100%'>
+                    <UserDetails/>
+                </Box>
+            </VStack>
+        </Flex>
+    )
+}
+
+const UserDetails = () => {
+    return (
+        <VStack w='100%' alignItems='start' >
+            <Text>User Details</Text>
+        </VStack>
+    )
 }
