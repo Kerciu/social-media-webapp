f6f504f89c81c00f4a48d0e22c4477d8c78893a9
Kerciu <kerciuuu@gmail.com>
2025-03-04 13:07:21 +0100

    base of user profile completed

--- frontend/src/routes/UserProfile.jsx
+++ frontend/src/routes/UserProfile.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, HStack, Text, VStack, Image } from "@chakra-ui/react"
+import { Box, Flex, Heading, HStack, Text, VStack, Image, Button } from "@chakra-ui/react"
 
 export const UserProfile = () => {
     return (
@@ -19,27 +19,30 @@ const UserDetails = () => {
     return (
         <VStack w='100%' alignItems='start' >
             <Heading>@Username</Heading>
-            <HStack>
+            <HStack gap='20px'>
                 <Box boxSize='150px' border='2px solid' borderColor='gray.300' borderRadius='full' overflow='hidden'>
                     <Image src='' alt='profile picture' borderRadius='full' boxSize='100%' objectFit='cover'/>
                 </Box>
-                <VStack>
-                    <HStack>
-                        <VStack>
+                <VStack gap='20px'>
+                    <HStack gap='20px' fontSize='18px'>
+                        <VStack gap='20px'>
                             <Text>Posts</Text>
                             <Text>0</Text>
                         </VStack>
-                        <VStack>
+                        <VStack gap='20px'>
                             <Text>Followers</Text>
                             <Text>0</Text>
                         </VStack>
-                        <VStack>
+                        <VStack gap='20px'>
                             <Text>Following</Text>
                             <Text>0</Text>
                         </VStack>
                     </HStack>
+                    <Button w='100%'>Edit Profile</Button>
                 </VStack>
             </HStack>
+
+            <Text fontSize='18px'>Hi, this is my BIO !!!</Text>
         </VStack>
     )
 }
