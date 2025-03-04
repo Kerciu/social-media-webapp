96b57afee65f74ee300cf673c9466852fe933b5b
Kerciu <kerciuuu@gmail.com>
2025-03-04 12:33:07 +0100

    created navbar component

---
+++ frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import { Flex, Text } from '@chakra-ui/react'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <Flex>
+        <Text>
+            Navbar
+        </Text>
+    </Flex>
+  )
+}
+
+export default Navbar
