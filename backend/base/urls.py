14af393da0eb635e36f7b61c0cea6d0feaa432d4
Kerciu <kerciuuu@gmail.com>
2025-03-04 11:59:42 +0100

    handle images endpoints

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,11 +1,12 @@
-from django.contrib import admin
 from django.urls import path, include
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 from posts.views import posts_list_view, post_details_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/posts/', posts_list_view),
     path('api/posts/<int:id>/', post_details_view),
     path('api/auth/', include('api.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
