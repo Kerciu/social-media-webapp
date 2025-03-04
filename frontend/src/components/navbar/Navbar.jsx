d80978f9b72968c8a31db4153de0db6da7c40b9d
Kerciu <kerciuuu@gmail.com>
2025-03-04 12:46:47 +0100

    finished making navbar for now

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,18 +1,31 @@
 import { Flex, HStack, Text } from '@chakra-ui/react'
-import React from 'react'
+import { FaUserCircle } from "react-icons/fa";
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 import { APP_NAME } from '../../utils/appName'
 
 const Navbar = () => {
-  return (
-    <Flex w='100vw' h='90px' bg='blue.500' justifyContent='center' alignItems='center'>
-        <HStack justifyContent='space-between' w='90%' color='white' fontSize={24} fontWeight='bold'>
-            <Text>{APP_NAME}</Text>
-            <HStack>
-                <Text>Profile</Text>
+
+    const [user, setUser] = useState('kacper')  // leave it for now
+    const nav = useNavigate()
+
+    const handleNavigate = (path) => {
+        nav(`/${path}`)
+    }
+
+    return (
+        <Flex w='100vw' h='90px' bg='blue.500' justifyContent='center' alignItems='center'>
+            <HStack justifyContent='space-between' w='90%' color='white' fontSize={24} fontWeight='bold'>
+                <Text>{APP_NAME}</Text>
+                <HStack>
+                    <Text onClick={() => handleNavigate(user)}>
+                        <FaUserCircle size='40px'/>
+                    </Text>
+                </HStack>
             </HStack>
-        </HStack>
-    </Flex>
-  )
+        </Flex>
+    )
 }
 
 export default Navbar
