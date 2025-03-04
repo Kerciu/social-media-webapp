125cc2dd9b59788d1f120b140e262c5ad661211d
Kerciu <kerciuuu@gmail.com>
2025-03-04 13:00:31 +0100

    created profile picture, post following and followers count fields

--- frontend/src/routes/userProfile.js
+++ frontend/src/routes/userProfile.js
@@ -1,10 +1,10 @@
-import { Box, Flex, Text, VStack } from "@chakra-ui/react"
+import { Box, Flex, Heading, HStack, Text, VStack, Image } from "@chakra-ui/react"
 
 export const UserProfile = () => {
     return (
         <Flex w='100%' justifyContent='center'>
             <VStack w='75%' >
-                <Box w='100%'>
+                <Box w='100%' mt='40px'>
                     <UserDetails/>
                 </Box>
             </VStack>
@@ -13,9 +13,33 @@ export const UserProfile = () => {
 }
 
 const UserDetails = () => {
+
+    const USER_IMAGE_PATH = 'http://localhost:8000/media/profile_image/default.jpg'
+
     return (
         <VStack w='100%' alignItems='start' >
-            <Text>User Details</Text>
+            <Heading>@Username</Heading>
+            <HStack>
+                <Box boxSize='150px' border='2px solid' borderColor='gray.300' borderRadius='full' overflow='hidden'>
+                    <Image src='' alt='profile picture' borderRadius='full' boxSize='100%' objectFit='cover'/>
+                </Box>
+                <VStack>
+                    <HStack>
+                        <VStack>
+                            <Text>Posts</Text>
+                            <Text>0</Text>
+                        </VStack>
+                        <VStack>
+                            <Text>Followers</Text>
+                            <Text>0</Text>
+                        </VStack>
+                        <VStack>
+                            <Text>Following</Text>
+                            <Text>0</Text>
+                        </VStack>
+                    </HStack>
+                </VStack>
+            </HStack>
         </VStack>
     )
 }
