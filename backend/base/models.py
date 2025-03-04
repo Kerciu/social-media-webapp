7cabfc244a52dce2c3665426e36221c2fe77fe1e
Kerciu <kerciuuu@gmail.com>
2025-03-04 11:50:44 +0100

    created custom user model

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=50, unique=True, primary_key=True)
+
+    bio = models.CharField(max_length=500, null=True, blank=True)
+
+    profile_img = models.ImageField(
+        upload_to='profile_images/',
+        null=True,
+        blank=True
+    )
+
+    followers_list = models.ManyToManyField(
+        'self',
+        blank=True,
+        symmetrical=False,
+        related_name='following'
+    )
+
+    def __str__(self):
+        return self.username
