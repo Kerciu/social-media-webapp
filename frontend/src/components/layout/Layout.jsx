3a84b956be0634d4c744c54425f83d36e960b31f
Kerciu <kerciuuu@gmail.com>
2025-03-04 12:34:07 +0100

    integrated navbar into the layout component

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -1,9 +1,12 @@
 import { Flex, VStack, Box } from '@chakra-ui/react'
 import React from 'react'
 
+import Navbar from '../navbar/Navbar'
+
 const Layout = ({ children }) => {
   return (
-    <VStack w='100vh' minH='100vh'>
+    <VStack w='100vw' minH='100vh'>
+        <Navbar/>
         <Box w='100%'>
             {children}
         </Box>
