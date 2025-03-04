6764b0bc97a88bfa39ff40cae1e8fc8b6e76d120
Kerciu <kerciuuu@gmail.com>
2025-03-04 12:41:09 +0100

    created skeleton of the navbar component

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,12 +1,16 @@
-import { Flex, Text } from '@chakra-ui/react'
+import { Flex, HStack, Text } from '@chakra-ui/react'
 import React from 'react'
+import { APP_NAME } from '../../utils/appName'
 
 const Navbar = () => {
   return (
-    <Flex>
-        <Text>
-            Navbar
-        </Text>
+    <Flex w='100vw' h='90px' bg='blue.500' justifyContent='center' alignItems='center'>
+        <HStack justifyContent='space-between' w='90%' color='white' fontSize={24} fontWeight='bold'>
+            <Text>{APP_NAME}</Text>
+            <HStack>
+                <Text>Profile</Text>
+            </HStack>
+        </HStack>
     </Flex>
   )
 }
