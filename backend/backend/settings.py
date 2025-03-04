4193ad3e2a6331ef65285f169225bd5964c1bad6
Kerciu <kerciuuu@gmail.com>
2025-03-04 15:24:39 +0100

    backend is now working along side with frontend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -185,5 +185,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWS_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://localhost:3001',
+]
+
+CORS_ALLOW_CREDENTIALS = True
