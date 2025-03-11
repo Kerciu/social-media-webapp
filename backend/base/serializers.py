91f21ee71f83b77c3b3c9c19029a64bf4ec0b48b
Kerciu <kerciuuu@gmail.com>
2025-03-11 12:33:41 +0100

    passing post id in post serializer

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -70,6 +70,7 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = [
+            'id',
             'username',
             'description',
             'formatted_date',
