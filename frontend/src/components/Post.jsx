fb0ba6e3d7991bf6fca25b16047229cce63fa1a2
Kerciu <kerciuuu@gmail.com>
2025-03-11 12:48:27 +0100

    fixed description and footer styling

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -15,8 +15,8 @@ const Post = (props) => {
             <Flex flex='6' w='100%' h='100%' justifyContent='center' alignItems='center'>
                 <Text textAlign='center'>{description}</Text>
             </Flex>
-            <Flex flex='2'>
-                <HStack>
+            <Flex flex='2' w='100%' justifyContent='center' alignItems='center' borderTop='1px solid' bg='gray.50' borderColor='gray.400' borderRadius='0 0 8px 8px'>
+                <HStack w='90%' justifyContent='space-between'>
                     <HStack>
                         <Button>Like</Button>
                         <Text>{like_count}</Text>
