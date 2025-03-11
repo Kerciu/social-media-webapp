2e533f64e2029568586a7e724ba5b0ea33bef955
Kerciu <kerciuuu@gmail.com>
2025-03-11 12:32:55 +0100

    made data flow in post look betteR

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,13 +1,18 @@
+import { VStack, Text } from '@chakra-ui/react'
 import React from 'react'
 
-const Post = ({username, description, formatted_date, like_count}) => {
-  return (<>
-    <div>{username}</div>
-    <div>{description}</div>
-    <div>{formatted_date}</div>
-    <div>{like_count}</div>
-    </>
-  )
+const Post = (props) => {
+
+    const { username, description, formatted_date, like_count } = props.post
+
+    return (
+        <VStack>
+            <Text>{username}</Text>
+            <Text>{description}</Text>
+            <Text>{formatted_date}</Text>
+            <Text>{like_count}</Text>
+        </VStack>
+    )
 }
 
 export default Post
