e6898c4b67dae5ed95ab8861d375f961dc183c96
Kerciu <kerciuuu@gmail.com>
2025-03-11 12:00:49 +0100

    created post model

--- backend/base/models.py
+++ backend/base/models.py
@@ -24,3 +24,24 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Post(models.Model):
+    user = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+
+    description = models.CharField(max_length=500)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    likes = models.ManyToManyField(
+        CustomUser,
+        blank=True,
+        related_name='post_likes'
+    )
+
+    def __str__(self):
+        return f'{self.user.username} - {self.description}'
