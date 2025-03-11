74948473878ba5d91c5be5e321912a1fd66da26e
Kerciu <kerciuuu@gmail.com>
2025-03-11 12:28:04 +0100

    fetching posts from backend and printing them on the user profile

--- frontend/src/components/UserPosts.jsx
+++ frontend/src/components/UserPosts.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import PostService from '../services/PostService'
+import { Flex, Box, Text } from '@chakra-ui/react'
 
 const UserPosts = ({ username }) => {
 
+    const [posts, setPosts] = useState([])
+    const [loading, setLoading] = useState(true)
 
+    useEffect(() => {
+        const fetchUserPosts = async () => {
+            try {
+                const response = await PostService.getPosts(username)
+                console.log(response)
+                setPosts(response)
+            } catch (error) {
+                console.log(error)
+            } finally {
+                setLoading(false)
+            }
+        }
 
-  return (
-    <></>
-  )
+        fetchUserPosts();
+    }, [])
+
+    return (
+        <Flex>
+            {
+                loading ? <Text>Loading...</Text> :
+                posts.map(post => (
+                    <Box key={post.id} w='100%' mt='40px'>
+                        <div>{post.description}</div>
+                    </Box>
+                ))
+            }
+        </Flex>
+    )
 }
 
 export default UserPosts
