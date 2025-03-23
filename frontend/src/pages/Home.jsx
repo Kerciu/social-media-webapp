bad89fe14e4603c0139f1e55eaf71732bbac0dbc
Kerciu <kerciuuu@gmail.com>
2025-03-23 16:59:15 +0100

    implement infinite scrolling and loading spinner in Home component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
-import { Flex, Heading, Text, VStack } from '@chakra-ui/react'
-import React, { useEffect, useState } from 'react'
+import { Flex, Heading, Text, VStack, Spinner } from '@chakra-ui/react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import PostService from '../services/PostService';
 import Post from '../components/Post';
 
@@ -10,25 +10,33 @@ const Home = () => {
     const [errorMessage, setErrorMessage] = useState("");
     const [nextPage, setNextPage] = useState(1)
 
-    useEffect(() => {
-        const fetchPosts = async () => {
-            try {
-                const response = await PostService.getHomepagePosts(nextPage);
-                console.log(response)
-                setPosts(response.results || [])
-            } catch (error) {
-                setErrorMessage(error.response || "An error occured")
-            } finally {
-                setLoading(false)
-            }
-        }
+    const [isFetchingMore, setFetchingMore] = useState(false);
+    const loadingRef = useRef(null)
 
+
+    const fetchPosts = useCallback(async () => {
+        try {
+            setFetchingMore(true)
+            const response = await PostService.getHomepagePosts(nextPage);
+            console.log(response)
+
+            setPosts((prevPosts) => [...prevPosts, ...(response.results || [])])
+            setNextPage((prevPage) => prevPage + 1)
+        } catch (error) {
+            setErrorMessage(error.response || "An error occured")
+        } finally {
+            setFetchingMore(false)
+            setLoading(false)
+        }
+    }, [nextPage])
+
+    useEffect(() => {
         fetchPosts();
-    }, [posts])
+    }, [])
 
     return (
-        <Flex w='100%' h='100%' justifyContent='center'>
-            <VStack>
+        <Flex w='100%' h='100%' justifyContent='center' pt='50px'>
+            <VStack alignItems='start' pb='50px' gap='30px'>
                 <Heading>Home</Heading>
                 {errorMessage && <Text>Error: {errorMessage}</Text>}
                 {
@@ -43,6 +51,9 @@ const Home = () => {
                     :
                     <Text>No posts to display...</Text>
                 }
+
+                <div ref={loadingRef} style={{ height: '50px' }} />
+                {isFetchingMore && <Spinner size="xl" />}
             </VStack>
         </Flex>
     )
