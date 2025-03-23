68d90b2c0674066d4818b67c9fdd1e67e04285a6
Kerciu <kerciuuu@gmail.com>
2025-03-23 16:04:30 +0100

    refactor CreatePost component layout and enhance input styling

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 
 const CreatePost = () => {
   return (
-    <Flex>
-        <VStack>
+    <Flex w='100%' h='100%' justifyContent='center' pt='50px'>
+        <VStack w='95%' maxW='450px' alignItems='start' gap='40px'>
             <Heading>Create Post</Heading>
             <FormControl>
                 <FormLabel>Description</FormLabel>
-                <Input/>
+                <Input bg='white' type='text' />
             </FormControl>
-            <Button>Submit</Button>
+            <Button w='100%' colorScheme='blue'>Submit</Button>
         </VStack>
     </Flex>
   )
