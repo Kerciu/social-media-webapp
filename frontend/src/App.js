4cd7fb5ac76d6690b28ec56ce9e7ea72cec709ac
Kerciu <kerciuuu@gmail.com>
2025-03-23 16:35:55 +0100

    add Home component and route to App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import Register from './pages/Register';
 import CreatePost from './pages/CreatePost';
 import { AuthProvider } from './contexts/AuthContext';
 import PrivateRoute from './routes/PrivateRoute';
+import Home from './pages/Home';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Routes>
             <Route path="/profile/:username" element={<Layout><PrivateRoute><UserProfile/></PrivateRoute></Layout>} />
             <Route path="/create-post" element={<Layout><PrivateRoute><CreatePost/></PrivateRoute></Layout>} />
+            <Route path="/" element={<Layout><PrivateRoute><Home/></PrivateRoute></Layout>} />
             <Route path="/login" element={<Layout><Login/></Layout>} />
             <Route path="/register" element={<Layout><Register/></Layout>} />
           </Routes>
